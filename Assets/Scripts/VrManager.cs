1c5ca3d9f967747cc66e6dd3a59e017e4e1640af
AlexSerodio <alequis01@gmail.com>
2020-04-06 17:12:48 -0300

    adicionado cena vr, menu principal e troca de cenas

---
+++ Assets/Scripts/VrManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class VrManager : MonoBehaviour {
+
+    [SerializeField] private bool vrModeEnabled;
+
+    void Start() {
+        XRSettings.enabled = vrModeEnabled;    
+    }
+
+}
